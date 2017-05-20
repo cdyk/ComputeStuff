a2fd950e4844e2e73f8b99f4953868d1cf640b7d
Christopher Dyken <cdyk@users.noreply.github.com>
2017-05-20 22:24:00 +0200

    Scaffolding

---
+++ HP5/HP5.cu
@@ -0,0 +1,4 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include "HP5.h"
---
+++ HP5Test/main.cu
@@ -0,0 +1,8 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+#include <HP5.h>
+
+int main()
+{
+}
