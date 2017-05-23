01f05bd543966961a7fc5c81fd8aad6bac3b5094
Christopher Dyken <cdyk@users.noreply.github.com>
2017-05-24 00:00:14 +0200

    Updated readme and docs

--- HP5Test/main.cu
+++ HP5Test/main.cu
@@ -210,10 +210,10 @@ int main()
   }
 
 
-  for (uint64_t N = 1220; N < (uint64_t)(props.totalGlobalMem / (sizeof(uint32_t) * 4)); N = 3 * N + N / 3) {
-    //for (uint32_t m = 1; m < 10; m++) {
-      runCompactTest(static_cast<uint32_t>(N), 17);
-    //}
+  for (uint64_t N = 1; N < (uint64_t)(props.totalGlobalMem / (sizeof(uint32_t) * 4)); N = 3 * N + N / 3) {
+    for (uint32_t m = 1; m < 128; m *=2) {
+      runCompactTest(static_cast<uint32_t>(N), m);
+    }
   }
 
 }
